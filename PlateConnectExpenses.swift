3339e033c81f76f67a14ff274eca8ca8783a5835
ashvinj6 <132094318+ashvinj6@users.noreply.github.com>
2023-04-28 13:31:20 -0500

    Add files via upload

---
+++ PlateConnectExpenses.swift
@@ -0,0 +1,28 @@
+//
+//  PlateConnectExpenses.swift
+//  ExpenseTracker
+//
+//  Created by ashvin.
+//
+
+import SwiftUI
+
+@main
+struct PlateConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
